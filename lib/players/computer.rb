84fa04448616acd4fd444802f3054df9d5f14ce7
Kevin Chi <kevinchi118@gmail.com>
2018-07-01 01:53:47 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,7 +1,18 @@
 module Players
     class Computer < Player
         def move(board)
-            move = rand(1..9).to_s
+            # move = rand(1..9).to_s
+            WIN_COMBINATIONS = [
+                [0, 1, 2], [3, 4, 5], [6, 7, 8],
+                [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                [0, 4, 8], [2, 4, 6]
+              ]
+
+           # make winning move
+           
+
+           WIN_COMBINATIONS.each do |combination|
+               if board.
         end
     end
 end
