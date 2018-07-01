5b88427a8ae67a6a7e4f1e9e84a3191dd2f0df0a
Kevin Chi <kevinchi118@gmail.com>
2018-07-01 01:08:23 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,13 +1,7 @@
 module Players
     class Computer < Player
         def move(board)
-            valid_moves = []
-            board.cells.each_with_index do |cell, index|
-                if !board.taken?(cell)
-                    valid_moves << (index + 1).to_s
-                end
-            end
-            valid_moves
+            move = rand(1..9)
         end
     end
 
