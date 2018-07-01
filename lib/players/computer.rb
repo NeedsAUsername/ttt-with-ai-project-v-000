ef963fc3732afe689f1e37ca803fab5e25d2463d
Kevin Chi <kevinchi118@gmail.com>
2018-07-01 02:10:10 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,41 +1,48 @@
 module Players
     class Computer < Player
+        WIN_COMBINATIONS = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6]
+          ]
+
+
+
         def move(board)
             # move = rand(1..9).to_s
-            WIN_COMBINATIONS = [
-                [0, 1, 2], [3, 4, 5], [6, 7, 8],
-                [0, 3, 6], [1, 4, 7], [2, 5, 8],
-                [0, 4, 8], [2, 4, 6]
-              ]
+            winning_move? || blocking_move? || rand(1..9).to_s
+        end
 
+        def winning_move?
+            winning_move = nil
+            WIN_COMBINATIONS.each do |c|
+               if board.cells[c[0]] == self.opponent && board.cells[c[1]] == self.opponent && board.cells[c[2]] == " "
+                   winning_move = c[2]
+               elsif board.cells[c[0]] == self.opponent && board.cells[c[1]] == " " && board.cells[c[2]] == self.opponent
+                   winning_move = c[1]
+               elsif board.cells[c[0]] == " " && board.cells[c[1]] == self.opponent && board.cells[c[2]] == self.opponent
+                   winning_move = c[0]
+            end
+            winning_move
+        end
 
-           WINNING_MOVE = nil
-           BLOCKING_MOVE = nil
-           OPPONENT = "O"
+        def opponent
+            self.opponent == "X" ? "O" : "X"
+        end
 
-           if @token == "O"
-               OPPONENT = "X"
-           end
-           # check for winning moves
-           WIN_COMBINATIONS.each do |c|
-               if board.cells[c[0]] == @token && board.cells[c[1]] == @token && board.cells[c[2]] == " "
-                   WINNING_MOVE = c[2]
-               elsif board.cells[c[0]] == @token && board.cells[c[1]] == " " && board.cells[c[2]] == @token
-                   WINNING_MOVE = c[1]
-               elsif board.cells[c[0]] == " " && board.cells[c[1]] == @token && board.cells[c[2]] == @token
-                   WINNING_MOVE = c[0]
-               end
-           end
-
-
-           WIN_COMBINATIONS.each do 
-
-
-
-
-
-
-
+        def blocking_move?
+            blocking_move = nil
+            WIN_COMBINATIONS.each do |c|
+                if board.cells[c[0]] == self.opponent && board.cells[c[1]] == self.opponent && board.cells[c[2]] == " "
+                    winning_move = c[2]
+                elsif board.cells[c[0]] == self.opponent && board.cells[c[1]] == " " && board.cells[c[2]] == self.opponent
+                    winning_move = c[1]
+                elsif board.cells[c[0]] == " " && board.cells[c[1]] == self.opponent && board.cells[c[2]] == self.opponent
+                    winning_move = c[0]
+                end
+             end
+             blocking_move
+         end
         end
     end
 end
