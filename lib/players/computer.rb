4b0502b9369942830a51a3bd82c4c97febf03c1c
Kevin Chi <kevinchi118@gmail.com>
2018-07-01 00:56:33 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,7 +1,7 @@
 module Players
     class Computer < Player
         def move(board)
-            board.cells.detect {|cell| board.valid_move?(cell)}
+            board.cells.detect {|cell| self.valid_move?(cell)}
         end
     end
 
