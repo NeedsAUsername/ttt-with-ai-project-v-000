c05ef45113208eb08dffa79265ec15e41c031a84
Kevin Chi <kevinchi118@gmail.com>
2018-07-01 00:53:01 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,10 +1,7 @@
 module Players
     class Computer < Player
         def move(board)
-            random = rand(1..9)
-            if !board.valid_move?(random)
-                self.move(board)
-            end 
+            board.
         end
     end
 
