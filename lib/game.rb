6a2f96f632a827712de6e28a56bf221bd5f5be82
Kevin Chi <kevinchi118@gmail.com>
2018-07-01 00:21:25 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,13 @@
 class Game
+    attr_accessor :player_1, :player_2, :board
+
+    WIN_COMBINATIONS = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ]
 
 
-    WIN_COMBINATIONS = 
-
+  def initialize(player_1 = Player:Human.new, player_2, board = Board.new)
 
 end
